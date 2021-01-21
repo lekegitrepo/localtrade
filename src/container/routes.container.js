cc4e8bf63dd86bbb8dcef17a01e9c69ed5cb7c69
lekegitrepo <lekedevrepo@gmail.com>
2021-01-21 11:30:54 -0600

    add registration and login routes

--- src/container/routes.container.js
+++ src/container/routes.container.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../components/App';
+import Login from '../components/auth/Login';
+import Registration from '../components/auth/Registration';
 
 const PageRoutes = () => (
   <Router>
     <Switch>
       <Route exact path="/">
         <App />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/registration">
+        <Registration />
       </Route>
     </Switch>
   </Router>
